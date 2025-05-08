e672ac64cf98ce736353d4586b1e4ad1f11377dc
Wycliff Kimutai <99463181+kimutaiwycliff@users.noreply.github.com.>
2025-05-08 15:54:32 +0300

    styling modifications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Progress } from '@/components/ui/progress';
 
 const Home = () => {
   return (
-    <div>
-      <Button variant="default">Home</Button></div>
-  )
-}
-export default Home
+    <div className='flex flex-col gap-4'>
+      <p>Hello World</p>
+      <Button variant="elevated" className='max-w-xs'>Home</Button>
+      <Input placeholder='Enter your name'  className='w-1/2'/>
+      <Progress value={50} className='w-1/2' />
+    </div>
+  );
+};
+export default Home;
